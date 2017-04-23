f32f19ad15b6a562f688bf52744da149ad61f925
Paweł Piskorz <pawel@arabel.la>
2017-04-23 21:44:40 +0200

    ARD-47 PasswordFieldElement with tests

--- src/elements/index.js
+++ src/elements/index.js
@@ -3,11 +3,13 @@
 import ErrorElement from './ErrorElement';
 import LabelElement from './LabelElement';
 import EmailFieldElement from './EmailFieldElement';
+import PasswordFieldElement from './PasswordFieldElement';
 import TextFieldElement from './TextFieldElement';
 
 export {
   ErrorElement,
   LabelElement,
   EmailFieldElement,
+  PasswordFieldElement,
   TextFieldElement,
 };
