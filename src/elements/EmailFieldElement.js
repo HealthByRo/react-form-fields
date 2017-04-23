fe5e395897e438d972546e7b40ce4dfc58a8b036
Paweł Piskorz <pawel@arabel.la>
2017-04-23 21:30:05 +0200

    ARD-46 EmailFieldElement with tests

---
+++ src/elements/EmailFieldElement.js
@@ -0,0 +1,27 @@
+/* @flow */
+
+import React from 'react';
+import type { InputProps, TextFieldElementProps } from './types';
+
+const EmailFieldElement = (props: TextFieldElementProps) => {
+  const inputProps: InputProps = {
+    id: props.inputId,
+    type: 'email',
+    name: props.name,
+    className: props.className,
+  };
+
+  if (props.placeholder) {
+    inputProps.placeholder = props.placeholder;
+  }
+
+  return (
+    <input {...inputProps} />
+  );
+};
+// TODO figure out a better way of using defaults in stateless component
+EmailFieldElement.defaultProps = {
+  className: 'textfield',
+};
+
+export default EmailFieldElement;
--- src/elements/index.js
+++ src/elements/index.js
@@ -2,10 +2,12 @@
 
 import ErrorElement from './ErrorElement';
 import LabelElement from './LabelElement';
+import EmailFieldElement from './EmailFieldElement';
 import TextFieldElement from './TextFieldElement';
 
 export {
   ErrorElement,
   LabelElement,
+  EmailFieldElement,
   TextFieldElement,
 };
