e76f0df4ad9b026f1a2c5143d54a9e72d37b7d8a
Paweł Piskorz <pawel@arabel.la>
2017-04-25 13:33:07 +0200

    ARD-43 PR cleanup - test names

--- src/EmailField.test.js
+++ src/EmailField.test.js
@@ -5,97 +5,111 @@ import renderer from 'react-test-renderer';
 import EmailField from './EmailField';
 import type { FieldProps } from './types';
 
+let tree;
+const placeholder = 'INPUT_PLACEHOLDER';
+const label = 'INPUT_LABEL';
+const renderWithProps = (props: FieldProps) => {
+  tree = renderer.create(
+    <EmailField {...props} />
+  ).toJSON();
+};
+const getPropsWithName = (props?: Object, name?: string = 'INPUT_NAME'): FieldProps => {
+  const propsWithName = { ...props };
+
+  if (!propsWithName.name) {
+    propsWithName.name = name;
+  }
+
+  return propsWithName;
+};
+
 describe('<EmailField />', () => {
-  let tree;
-  const placeholder = 'INPUT_PLACEHOLDER';
-  const label = 'INPUT_LABEL';
-  const renderWithProps = (props: FieldProps) => {
-    tree = renderer.create(
-      <EmailField {...props} />
-    ).toJSON();
-  };
-  const getPropsWithName = (props?: Object, name?: string = 'INPUT_NAME'): FieldProps => {
-    const propsWithName = { ...props };
+  it('should throw error if no name passed', () => {
+    // $FlowExpectedError
+    expect(() => renderWithProps()).toThrowError('name param is required for EmailField');
+  });
+});
 
-    if (!propsWithName.name) {
-      propsWithName.name = name;
-    }
-
-    return propsWithName;
-  };
-
-  describe('no params:', () => {
-    it('should throw error if no name passed', () => {
-      // $FlowExpectedError
-      expect(() => renderWithProps()).toThrowError('name param is required for EmailField');
-    });
+describe('<EmailField { name } />', () => {
+  it('should NOT throw any errors if name is passed', () => {
+    expect(() => <EmailField name="TEST_NAME" />).not.toThrowError();
   });
 
-  describe('param name:', () => {
-    it('should NOT throw any errors if name is passed', () => {
-      expect(() => <EmailField name="TEST_NAME" />).not.toThrowError();
-    });
+  it('should render only wrapped input field with defaults', () => {
+    renderWithProps(getPropsWithName());
 
-    it('should render only wrapped input field with defaults', () => {
-      renderWithProps(getPropsWithName());
-
-      expect(tree).toMatchSnapshot();
-    });
+    expect(tree).toMatchSnapshot();
   });
+});
 
-  describe('multiple params', () => {
-    it('name + placeholder', () => {
-      renderWithProps(getPropsWithName({ placeholder }));
+describe('<EmailField { name, placeholder } />', () => {
+  it('should render input with placeholder', () => {
+    renderWithProps(getPropsWithName({ placeholder }));
 
-      expect(tree).toMatchSnapshot();
-    });
+    expect(tree).toMatchSnapshot();
+  });
+});
 
-    it('name + placeholder + errors[1]', () => {
-      renderWithProps(getPropsWithName({ placeholder, errors: ['SINGLE_ERROR'] }));
+describe('<EmailField { name, placeholder, errors: [SINGLE_ERROR] } />', () => {
+  it('should render input with placeholder and single error', () => {
+    renderWithProps(getPropsWithName({ placeholder, errors: ['SINGLE_ERROR'] }));
 
-      expect(tree).toMatchSnapshot();
-    });
+    expect(tree).toMatchSnapshot();
+  });
+});
 
-    it('name + placeholder + errors[2]', () => {
-      renderWithProps(getPropsWithName({ placeholder, errors: ['FIRST_ERROR', 'SECOND_ERROR'] }));
+describe('<EmailField { name, placeholder, errors: [FIRST_ERROR, SECOND_ERROR] } />', () => {
+  it('should render input with placeholder and two errors', () => {
+    renderWithProps(getPropsWithName({ placeholder, errors: ['FIRST_ERROR', 'SECOND_ERROR'] }));
 
-      expect(tree).toMatchSnapshot();
-    });
+    expect(tree).toMatchSnapshot();
+  });
+});
 
-    it('name + label', () => {
-      renderWithProps(getPropsWithName({ label }));
+describe('<EmailField { name, label } />', () => {
+  it('should render input and labbel', () => {
+    renderWithProps(getPropsWithName({ label }));
 
-      expect(tree).toMatchSnapshot();
-    });
+    expect(tree).toMatchSnapshot();
+  });
+});
 
-    it('name + label + errors[1]', () => {
-      renderWithProps(getPropsWithName({ label, errors: ['SINGLE_ERROR'] }));
+describe('<EmailField { name, label, errors: [SINGLE_ERROR] } />', () => {
+  it('should render input and label and single error', () => {
+    renderWithProps(getPropsWithName({ label, errors: ['SINGLE_ERROR'] }));
 
-      expect(tree).toMatchSnapshot();
-    });
+    expect(tree).toMatchSnapshot();
+  });
+});
 
-    it('name + label + errors[2]', () => {
-      renderWithProps(getPropsWithName({ label, errors: ['FIRST_ERROR', 'SECOND_ERROR'] }));
+describe('<EmailField { name, label, errors: [FIRST_ERROR, SECOND_ERROR] } />', () => {
+  it('should render input and label and two errors', () => {
+    renderWithProps(getPropsWithName({ label, errors: ['FIRST_ERROR', 'SECOND_ERROR'] }));
 
-      expect(tree).toMatchSnapshot();
-    });
+    expect(tree).toMatchSnapshot();
+  });
+});
 
-    it('name + label + placeholder', () => {
-      renderWithProps(getPropsWithName({ label, placeholder }));
+describe('<EmailField { name, placeholder, label } />', () => {
+  it('should render input with placeholder and label', () => {
+    renderWithProps(getPropsWithName({ label, placeholder }));
 
-      expect(tree).toMatchSnapshot();
-    });
+    expect(tree).toMatchSnapshot();
+  });
+});
 
-    it('name + label + placeholder + errors[1]', () => {
-      renderWithProps(getPropsWithName({ label, placeholder, errors: ['SINGLE_ERROR'] }));
+describe('<EmailField { name, placeholder, label, errors: [SINGLE_ERROR] } />', () => {
+  it('should render input with placeholder and label and single error', () => {
+    renderWithProps(getPropsWithName({ label, placeholder, errors: ['SINGLE_ERROR'] }));
 
-      expect(tree).toMatchSnapshot();
-    });
+    expect(tree).toMatchSnapshot();
+  });
+});
 
-    it('name + label + placeholder + errors[2]', () => {
-      renderWithProps(getPropsWithName({ label, placeholder, errors: ['FIRST_ERROR', 'SECOND_ERROR'] }));
+describe('<EmailField { name, placeholder, label, errors: [FIRST_ERROR, SECOND_ERROR] } />', () => {
+  it('should render input with placeholder and label and two errors', () => {
+    renderWithProps(getPropsWithName({ label, placeholder, errors: ['FIRST_ERROR', 'SECOND_ERROR'] }));
 
-      expect(tree).toMatchSnapshot();
-    });
+    expect(tree).toMatchSnapshot();
   });
 });
