55a2329d3c68bda8d899b66c6964e91d0125de08
Andrzej Dybionka <andrzej@arabel.la>
2017-06-02 14:37:55 +0200

    Map immutable object to JS object

--- src/createReduxFormField.js
+++ src/createReduxFormField.js
@@ -4,42 +4,59 @@
 *
 */
 
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { isCollection } from 'immutable';
 import _isArray from 'lodash/isArray';
 
 export default function createReduxFormField(InputComponent) {
-  return class WrappedReduxFormFieldComponent extends Component { // eslint-disable-line react/prefer-stateless-function
-    static propTypes = {
-      input: PropTypes.object.isRequired,
-      meta: PropTypes.object.isRequired,
-    };
+  function ReduxFormField(props) { // eslint-disable-line react/prefer-stateless-function
+    const {
+      input,
+      meta,
+      meta: { asyncValidating },
+      ...fieldProps
+    } = props;
+    const errors = getErrorsFromMeta(meta);
+    const processedInput = mapInputValue(input);
 
-    getErrors() {
-      const { error, touched } = this.props.meta;
+    return (
+      <div className={asyncValidating ? 'async-validating' : ''}>
+        <InputComponent
+          errors={errors}
+          {...processedInput}
+          {...fieldProps}
+        />
+      </div>);
+  }
 
-      if (error && touched) {
-        return _isArray(error) ? error : [error];
-      }
-
-      return null;
-    }
-
-    render() {
-      const {
-        input,
-        meta: { asyncValidating },
-        ...fieldProps
-      } = this.props;
-
-      return (
-        <div className={asyncValidating ? 'async-validating' : ''}>
-          <InputComponent
-            errors={this.getErrors()}
-            {...input}
-            {...fieldProps}
-          />
-        </div>);
-    }
+  ReduxFormField.propTypes = {
+    input: PropTypes.object.isRequired,
+    meta: PropTypes.object.isRequired,
   };
+
+  return ReduxFormField;
+}
+
+function getErrorsFromMeta(meta) {
+  const {
+    error,
+    touched,
+  } = meta;
+
+  if (error && touched) {
+    return _isArray(error) ? error : [error];
+  }
+
+  return null;
+}
+
+function mapInputValue(input) {
+  const processedInput = { ...input };
+
+  if (isCollection(processedInput.value)) {
+    processedInput.value = processedInput.value.toJS();
+  }
+
+  return processedInput;
 }
